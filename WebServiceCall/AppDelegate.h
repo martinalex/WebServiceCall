4c3e68b63f94b574764d45a63f7a91c0d95953d5
Martin Rosas <marlex@Dolomiti.local>
2012-08-15 12:35:13 -0700

    Initial Commit

---
+++ WebServiceCall/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WebServiceCall
+//
+//  Created by Martin Rosas on 8/15/12.
+//  Copyright (c) 2012 Martin Rosas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WebServiceCall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebServiceCall
+//
+//  Created by Martin Rosas on 8/15/12.
+//  Copyright (c) 2012 Martin Rosas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
